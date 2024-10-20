a4912a3d7046657dd670265e7266212977b35274
Kainy <kainykawai@gmail.com>
2024-10-20 20:38:13 +0300

    Feat : practicing encoding

--- index.py
+++ index.py
@@ -5,12 +5,20 @@ import pandas as pd
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-print(X)
-print(y)
+# print(X)
+# print(y)
 
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
+# print(X)
+ 
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+
+X = np.array(ct.fit_transform(X))
 print(X)
